39f66215c49df6cb780ee27ec16b4bec695ed75d
felipeadamoli <felipeadamoli@gmail.com>
2018-04-16 15:54:50 -0300

    refatorando listagem de tweets

--- js/TweetList.js
+++ js/TweetList.js
@@ -4,19 +4,11 @@ import { fetchTweets } from './services/services';
 export default class TweetList {
     constructor(props) {
         this.container = document.getElementById(props.tweetsContainer);
-        this.getTweets = this.getTweets.bind(this);
-    }
-
-    getTweets() {
-        return fetchTweets()
-            .then((resp) => {
-                this.tweets = resp;
-            })
     }
 
     async render() {
         try {
-            await this.getTweets();
+            this.tweets = await fetchTweets();
             const html = this.tweets.map(t => {
                 const tweet = new Tweet(t);
                 return tweet.render();
